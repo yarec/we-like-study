3326523a36b48721006cbdeee78307c5e8489f9f
wei1224hf@gmail.com <wei1224hf@gmail.com@ed97df9f-4d44-95dc-24c6-406f5635cf3e>
2013-10-19 13:01:19 +0000

    发现并纠正了一些小错误

--- trunk/ we-like-study --username wei1224hf@gmail.com/wls201310/wls201310/config.xml.php
+++ trunk/ we-like-study --username wei1224hf@gmail.com/wls201310/wls201310/config.xml.php
@@ -5,7 +5,7 @@
 <item ID="DB_NAME">wls</item>
 <item ID="DB_UNM">root</item>
 <item ID="DB_PWD"></item>
-<item ID="DB_HOST">localhost:3306:3306</item>
+<item ID="DB_HOST">localhost:3306</item>
 <item ID="MODE">WLS</item>
 <item ID="DB_URL" Explanation="JAVA服务端所用的连接参数">jdbc:mysql://localhost:3306/db_4?characterEncoding=utf8</item>
 <item ID="ZONE" Explanation="行政区划编码,参考国标文件 ,2位长度为省 4位长度为地级市6位长度为县级市,地级市所模拟出来的数据会达到800万,因此最好用 县级市 来模拟">330281</item>
